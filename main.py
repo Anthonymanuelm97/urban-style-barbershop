4c473e342894660b05470f6ae40134aa9f7e2f73
Anthonymanuelm97 <Anthonymanuelm97@gmail.com>
2026-09-08 18:13:46 -0400

    feat: add manual welcome message logic

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from urban-style-barbershop!")
+name = "Carlos"
+age = 30
+height = 1.75
+is_client = True
+
+print(name)
+print(type(name))
+
+business_name = "Urban Style Barbershop"
+haircut_price = 350
+discount_percentage = 15
+frequent_client = False
+
+discounted_price = haircut_price - (haircut_price * discount_percentage / 100)
+
+if frequent_client:
+    message = (
+        f"Welcome back to {business_name}! "
+        f"Your haircut costos ${discounted_price:.2f} "
+        f"with your frequent client discount. "
+    )
+else:
+    message = (
+        f"Welcome to {business_name}! "
+        f"Your haircut costs ${haircut_price:.2f}. "
+    )
+
+print(message)
 
 
-if __name__ == "__main__":
-    main()
